d8370e6d8a15d101c4bf205501fb2b08fcc6984d
温婧 <j-wen15@mails.tsinghua.edu.cn>
2017-10-22 16:38:19 +0800

    rm init

--- src/rm.h
+++ src/rm.h
@@ -1,101 +1,83 @@
-//
-// rm.h
-//
-//   Record Manager component interface
-//
-// This file does not include the interface for the RID class.  This is
-// found in rm_rid.h
-//
-
 #ifndef RM_H
 #define RM_H
 
-// Please DO NOT include any files other than redbase.h and pf.h in this
-// file.  When you submit your code, the test program will be compiled
-// with your rm.h and your redbase.h, along with the standard pf.h that
-// was given to you.  Your rm.h, your redbase.h, and the standard pf.h
-// should therefore be self-contained (i.e., should not depend upon
-// declarations in any other file).
+#include <cerrno>
+#include <iostream>
+#include "pm.h"
 
-// Do not change the following includes
-#include "redbase.h"
-#include "rm_rid.h"
-#include "pf.h"
-
-//
-// RM_Record: RM Record interface
-//
-class RM_Record {
-public:
-    RM_Record ();
-    ~RM_Record();
-
-    // Return the data corresponding to the record.  Sets *pData to the
-    // record contents.
-    RC GetData(char *&pData) const;
-
-    // Return the RID associated with the record
-    RC GetRid (RID &rid) const;
-};
-
-//
-// RM_FileHandle: RM File interface
-//
-class RM_FileHandle {
-public:
-    RM_FileHandle ();
-    ~RM_FileHandle();
-
-    // Given a RID, return the record
-    RC GetRec     (const RID &rid, RM_Record &rec) const;
-
-    RC InsertRec  (const char *pData, RID &rid);       // Insert a new record
-
-    RC DeleteRec  (const RID &rid);                    // Delete a record
-    RC UpdateRec  (const RM_Record &rec);              // Update a record
-
-    // Forces a page (along with any contents stored in this class)
-    // from the buffer pool to disk.  Default value forces all pages.
-    RC ForcePages (PageNum pageNum = ALL_PAGES);
-};
-
-//
-// RM_FileScan: condition-based scan of records in the file
-//
-class RM_FileScan {
-public:
-    RM_FileScan  ();
-    ~RM_FileScan ();
-
-    RC OpenScan  (const RM_FileHandle &fileHandle,
-                  AttrType   attrType,
-                  int        attrLength,
-                  int        attrOffset,
-                  CompOp     compOp,
-                  void       *value,
-                  ClientHint pinHint = NO_HINT); // Initialize a file scan
-    RC GetNextRec(RM_Record &rec);               // Get next matching record
-    RC CloseScan ();                             // Close the scan
-};
-
-//
-// RM_Manager: provides RM file management
-//
 class RM_Manager {
+
+private:
+	PF_Manager &pfm;
+
 public:
-    RM_Manager    (PF_Manager &pfm);
-    ~RM_Manager   ();
-
-    RC CreateFile (const char *fileName, int recordSize);
-    RC DestroyFile(const char *fileName);
-    RC OpenFile   (const char *fileName, RM_FileHandle &fileHandle);
-
-    RC CloseFile  (RM_FileHandle &fileHandle);
+	RM_Manager (PF_Manager &_pfm); // Constructor 
+	~RM_Manager (); // Destructor 
+	
+	RC CreateFile (const char *fileName, int recordSize); // Create a new file 
+	RC DestroyFile (const char *fileName); // Destroy a file 
+	RC OpenFile (const char *fileName, RM_FileHandle &fileHandle); // Open a file 
+	RC CloseFile (RM_FileHandle &fileHandle); // Close a file
 };
 
-//
-// Print-error function
-//
-void RM_PrintError(RC rc);
+class RM_FileHandle { 
+
+private:
+	bool attachedFile;
+	PF_FileHandle *fileHandle;
+	RM_FileHeader fileHeader;
+
+public: 
+	RM_FileHandle (); // Constructor 
+	~RM_FileHandle (); // Destructor 
+
+	RC AttachFile(PF_FileHandle pffh);
+	RC UnattachFile(PF_FileHandle pffh);
+
+	RC GetRec (const RID &rid, RM_Record &rec) const; // Get a record 
+	RC InsertRec (const char *pData, RID &rid); // Insert a new record, return record id 
+	RC DeleteRec (const RID &rid); // Delete a record 
+	RC UpdateRec (const RM_Record &rec); // Update a record 
+	RC ForcePages (PageNum pageNum = ALL_PAGES) const; // Write dirty page(s) to disk 
+};
+
+class RM_FileScan { 
+
+public: 
+	RM_FileScan (); // Constructor 
+	~RM_FileScan (); // Destructor 
+
+	RC OpenScan (const RM_FileHandle &fileHandle, // Initialize file scan 
+				 AttrType attrType, 
+				 int attrLength, 
+				 int attrOffset, 
+				 CompOp compOp, 
+				 void *value, 
+				 ClientHint pinHint = NO_HINT); 
+	RC GetNextRec (RM_Record &rec); // Get next matching record 
+	RC CloseScan (); // Terminate file scan 
+};
+
+class RM_Record { 
+
+	RID rid;
+	char *pdata;
+
+public: 
+	RM_Record (); // Constructor 
+	~RM_Record (); // Destructor 
+
+	RC GetData (char *&pData) const; // Set pData to point to the record's contents 
+	RC GetRid (RID &rid) const; // Get the record id 
+};
+
+
+struct RM_FileHeader {
+	PageNum freePageHead;
+};
+
+struct RM_PageHeader {
+
+};
 
 #endif
--- src/rm_rid.h
+++ src/rm_rid.h
@@ -1,40 +1,13 @@
-//
-// rm_rid.h
-//
-//   The Record Id interface
-//
+class RID { 
 
-#ifndef RM_RID_H
-#define RM_RID_H
+	const static int NULL_PAGE_NUM = -1;
+	const static int NULL_SLOT_NUM = -1;
 
-// We separate the interface of RID from the rest of RM because some
-// components will require the use of RID but not the rest of RM.
+public: 
+	RID (); // Default constructor 
+	~RID (); // Destructor 
 
-#include "redbase.h"
-
-//
-// PageNum: uniquely identifies a page in a file
-//
-typedef int PageNum;
-
-//
-// SlotNum: uniquely identifies a record in a page
-//
-typedef int SlotNum;
-
-//
-// RID: Record id interface
-//
-class RID {
-public:
-    RID();                                         // Default constructor
-    RID(PageNum pageNum, SlotNum slotNum);
-    ~RID();                                        // Destructor
-
-    RC GetPageNum(PageNum &pageNum) const;         // Return page number
-    RC GetSlotNum(SlotNum &slotNum) const;         // Return slot number
-
-private:
+	RID (PageNum pageNum, SlotNum slotNum); // Construct RID from page and slot number 
+	RC GetPageNum (PageNum &pageNum) const; // Return page number 
+	RC GetSlotNum (SlotNum &slotNum) const; // Return slot number 
 };
-
-#endif
